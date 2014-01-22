1df98948e8bf46e42b7aec973aa785c683f437e5
Flurin Egger <flurin@digitpaint.nl>
2014-01-22 22:24:56 +0100

    Acutally add raspicam to the server

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ var http = require('http')
 var express = require('express');
 var binaryjs = require('binaryjs');
 var fs = require('fs');
+var RaspiCam = require("raspicam");
+
+
 
 var BinaryServer = binaryjs.BinaryServer;
 var app = express();
+var camera = new RaspiCam({ 
+  mode: "photo"
+});
+
+var sendFileToAllClients = function(filename){
+  var cs = binaryserver.clients;  
+  var file = fs.createReadStream(filename);
+  Object.keys(cs).forEach(function(k){
+    cs[k].send(file);
+  })  
+}
 
 // Let's serve static files
 app.use(express.static(__dirname + '/public'));
@@ -23,6 +37,16 @@ binaryserver.on('connection', function(client){
 });
 
 app.get("/snap", function(req, res){
+  camera.start();
+  res.send("Taken photo");
+})
+
+camera.on("read", function(err, filename){ 
+  console.log("Took photo ", filename);
+  sendFileToAllClients(filename);
+});
+
+app.get("/snap-test", function(req, res){
   var o = [];
   var cs = binaryserver.clients;
   var file = fs.createReadStream(__dirname + '/test.png');
@@ -33,6 +57,9 @@ app.get("/snap", function(req, res){
   
   res.send(JSON.stringify(o));
 })
+
+
+
 
 server.listen(port);
 console.log('Listening on port ' + port);
